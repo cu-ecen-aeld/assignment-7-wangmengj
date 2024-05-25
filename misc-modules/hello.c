b6de1c787c4cf4b1ba05fd7b67eb5a712517f400
wangmengj <wangmengj@gmail.com>
2024-05-25 09:07:52 -0700

    assignment 7 part2

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,7 +7,8 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	//printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world! -- wangmengj\n");
 	return 0;
 }
 
